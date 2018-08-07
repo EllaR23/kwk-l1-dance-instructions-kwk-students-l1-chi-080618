a0ed6c891e0cb66f9b93e5f57c6facffacf9f1b2
EllaR23 <ellaratarac@gmail.com>
2018-08-07 15:40:40 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,3 +1,12 @@
 def say_hello(your_name, my_name = "Ella")
   puts "Hello, #{your_name}! I'm #{my_name}."
 end
+
+def about_me
+  puts "My name is Ella Ratarac."
+  puts "My middle name is Bleu, which is blue in French, even though I'm not even French"
+  puts "But, Blue is my favorite color so coincidence? I think not!"
+  puts "I have a really cute dog name Gable who my family adopted and has reddish brown longish fur, a face and body the shape of a golden retriever, and stubby legs."
+  puts "I have a fruit and green tea obsession."
+  puts "I also play field hockey!"
+end
