544f7b4e5d14b2528119084e8dfa18d34bd70446
EllaR23 <ellaratarac@gmail.com>
2018-08-07 15:32:39 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -2,7 +2,7 @@ def about_me
   puts "My name is Ella Ratarac."
   puts "My middle name is Bleu, which is blue in French, even though I'm not even French"
   puts "But, Blue is my favorite color so coincidence? I think not!"
-  puts "I have a really cute dog name Gable who my family adopted and is a mix of a golden retriever, irish setter, and shitzu so he has reddish brown longish fur, a face and body the shape of a golden retriever, and stubby legs."
-  puts "I have a fruit and green tea obsession. I also love weird healthy foods like chia, if you've ever heard of that."
+  puts "I have a really cute dog name Gable who my family adopted and has reddish brown longish fur, a face and body the shape of a golden retriever, and stubby legs."
+  puts "I have a fruit and green tea obsession."
   puts "I also play field hockey!"
 end
