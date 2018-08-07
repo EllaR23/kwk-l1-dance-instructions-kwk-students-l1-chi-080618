8040361013be5d911679e20cc52168d671d603bb
EllaR23 <ellaratarac@gmail.com>
2018-08-07 15:48:07 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,3 +1,10 @@
+def say_hello(your_name, my_name = "Dory")
+  puts "Hello #{your_name}! I'm #{my_name}"
+end
+
+
+
+
 def say_hello(your_name, my_name = "Ella")
   puts "Hello, #{your_name}! I'm #{my_name}."
 end
